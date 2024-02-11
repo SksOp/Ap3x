358f52955a44cb5f4480a71cbfb77e570a56ab2b
SksOp <shubhaman47@gmail.com>
2024-02-11 20:20:50 +0530

    completed research page

--- src/sections/research/index.tsx
+++ src/sections/research/index.tsx
@@ -1,7 +1,59 @@
 import Banner from "@/components/banner";
-import CardSections from "@/components/research";
+import ResearchView, { ResearchData } from "@/components/research";
 import SearchBar from "@/components/searchBar";
 import Container from "@/components/ui/container";
+
+const research: ResearchData[] = [
+  {
+    title: "Lorem Ipsum title ",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "12th June 2021",
+    link: "https://medium.com/",
+  },
+  {
+    title: "Lorem Ipsum title ",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "12th June 2021",
+    link: "https://medium.com/",
+  },
+  {
+    title: "Lorem Ipsum title ",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "12th June 2021",
+    link: "https://medium.com/",
+  },
+  {
+    title: "Lorem Ipsum title ",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "12th June 2021",
+    link: "https://medium.com/",
+  },
+  {
+    title: "Lorem Ipsum title ",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "12th June 2021",
+    link: "https://medium.com/",
+  },
+  {
+    title: "Lorem Ipsum title ",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "12th June 2021",
+    link: "https://medium.com/",
+  },
+  {
+    title: "Lorem Ipsum title ",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "12th June 2021",
+    link: "https://medium.com/",
+  },
+];
 
 function Research() {
   return (
@@ -9,7 +61,7 @@ function Research() {
       <>
         <Banner text="Research" />
         <SearchBar />
-        <CardSections />
+        <ResearchView data={research} />
       </>
     </Container>
   );
