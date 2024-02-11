1ea44263e29a70618cb58b8df7449edd49c4993f
SksOp <shubhaman47@gmail.com>
2024-02-11 19:42:54 +0530

    removed research section

---
+++ src/components/research.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { Card } from "./ui/card";
+
+function Research() {
+  return <div className="justify-center items-center"></div>;
+}
+
+export default Research;
--- src/sections/research/index.tsx
+++ src/sections/research/index.tsx
@@ -1,18 +1,18 @@
-import Banner from "@/components/banner"
-import CardSections from "@/components/cardSections"
-import SearchBar from "@/components/searchBar"
-import Container from "@/components/ui/container"
+import Banner from "@/components/banner";
+import CardSections from "@/components/research";
+import SearchBar from "@/components/searchBar";
+import Container from "@/components/ui/container";
 
 function Research() {
   return (
-    <Container>
-    <>  
-        <Banner text="Research"/>
+    <Container className="px-4">
+      <>
+        <Banner text="Research" />
         <SearchBar />
         <CardSections />
-    </>
-    </ Container>
-  )
+      </>
+    </Container>
+  );
 }
 
-export default Research
+export default Research;
