4268fa5dec31b5eccd00f64183c3852d18f66da6
Shekhar333 <shekharshivam333@gmail.com>
2024-02-11 16:22:10 +0530

    updated mountain view

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls, useGLTF ,  PerspectiveCamera } from '@react-three/drei';
 import { Button } from "./ui/button";
 import { ClassValue } from 'clsx';
 import { cn } from '@/lib/utils';
@@ -8,14 +8,14 @@ import { cn } from '@/lib/utils';
 function Intro() {
   return (
     // <div className="bg-[url('../public/parallax.png')] w-full h-[90vh] bg-no-repeat bg-cover bg-center bg-fixed">
-      <div className="relative flex flex-col items-center justify-center h-screen text-white">
-        <h1 className="text-6xl md:text-8xl xl:text-[12rem] text-center font-bold">
+      <div className="relative w-full  flex flex-col items-center justify-center h-[110vh] text-white">
+        <h1 className="relative z-[1] text-6xl md:text-8xl xl:text-[12rem] text-center font-bold">
           AP3X CAPITAL
         </h1>
-        <Button variant="secondary" size="default" className="mt-4">
+        <Button variant="secondary" size="default" className="relative z-[1] mt-4">
           Get Started
         </Button>
-        <World/>
+        <World className="absolute z-0" />
       </div>
     // </div>
   );
@@ -47,16 +47,17 @@ function World({className}:{  className?:ClassValue}) {
     // <div style={{backgroundColor: "white", height: '90vh', width: '100%', overflow: 'hidden' }}>
     <div className={cn("bg-white h-screen w-full overflow-hidden ",className)}>
       {/* Set overflow to 'hidden' to prevent scrolling */}
-      <Canvas camera={{ position: [400, 0, 500], fov: 15 }}>
+      <Canvas>
         {/* Set camera position and field of view */}
+        <PerspectiveCamera makeDefault position={[25, 0,350]} fov={40} near={0.1} far={2000} />
         <ambientLight intensity={0.1} />
         <directionalLight position={[5, 0, 5]} color={"#E3E8EF"} intensity={2} />
         <directionalLight position={[-5, 0, 5]} color={"#E3E8EF"} intensity={2} />
-        <group rotation={[10,0,0]} position={[.1,-1.1,0]}>
+        <group rotation={[4.86,4.7,4.75]} >
         <Model url={"mountain.glb"}/>
         {/* <Model url={"globe.glb"} position={modelPosition}/> */}
         </group>
-        <OrbitControls enableZoom={false} rotateSpeed={0}/>
+        <OrbitControls enableZoom={false}/>
         {/* Use OrbitControls for user interaction */}
       </Canvas>
     </div>
