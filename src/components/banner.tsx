9aa0f2fd7fe001afe3d6ed084fb4b5042fe4f472
SksOp <shubhaman47@gmail.com>
2024-02-15 22:52:15 +0530

    added cards in the intro section on home page

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -24,17 +24,4 @@ const Banner = ({ text }: { text: string }) => {
 
 export default Banner;
 
-// const Banner = ({text}:{ text:string }) => {
-//   return (
-//     <div className="relative mb-4">
-//     <div className="bg-gray-300 w-full h-40 flex justify-center items-center">
-//       <img src="./barcode.svg" alt="barcode" className="absolute inset-0 object-cover w-full h-full" />
-//     </div>
-//     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-7xl md:text-10xl font-bold uppercase">
-//         {text}
-//     </div>
-//   </div>
-//   );
-// };
-
 // export default Banner;
