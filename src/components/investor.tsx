9f9229fa5c4c26fd802dc82bf4145ef143bc44b5
SksOp <shubhaman47@gmail.com>
2024-02-16 00:43:34 +0530

    ui fixes

--- src/components/investor.tsx
+++ src/components/investor.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Slant } from "./svgs/slant";
 import { Button } from "./ui/button";
+import { paths } from "@/router";
 
 export interface Investor {
   id: number;
@@ -16,14 +17,14 @@ const Investor: React.FC<InvestorsProps> = ({ investors }) => {
   return (
     <div className="flex flex-col gap-5 items-center py-4 relative bg-white">
       <Slant className="absolute top-0 w-full translate-y-[-49%] " />
-      <h1 className="text-xl font-bold mb-4">Portfolio</h1>
-      <div className="grid grid-cols-4 gap-20 w-[50vw] ">
+      <p className="text-2xl font-bold mb-4">Portfolio</p>
+      <div className="grid gap-2 grid-cols-4 w-[50vw] ">
         {investors.map((investor, index) => (
           <div key={investor.id + index} className="flex justify-center">
             <a className="" href={investor.href ? investor.href : "#"}>
               <img
                 draggable={false}
-                className="w-16 h-auto hover:scale-125 grayscale hover:grayscale-0 transition-all duration-300 ease-in-out"
+                className="max-w-16 w-full h-auto hover:scale-125 grayscale hover:grayscale-0 transition-all duration-300 ease-in-out"
                 src={investor.logoLink}
                 alt={`${index}`}
               />
@@ -31,9 +32,9 @@ const Investor: React.FC<InvestorsProps> = ({ investors }) => {
           </div>
         ))}
       </div>
-      <a className="my-5" href="/investments">
-        <Button className="inline-flex items-center m-4 p-4 h-5">
-          <span className="p-1">View More</span>
+      <a className="my-5" href={paths.investments}>
+        <Button className="inline-flex items-center bg-foreground text-background hover:bg-foreground/70 text-lg m-4 p-4 h-5">
+          View More
         </Button>
       </a>
     </div>
