b53e93a56783b172145980ab9b0f53ece79b66f0
Ayush Kaithwas <ayush.91011@gmail.com>
2024-02-16 00:11:38 +0530

    Update Minimal Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,46 +4,16 @@ import Container from "./ui/container";
 
 function Footer() {
   return (
-    <footer className="p-10 bg-white">
-      <Container className="justify-center bg-white items-center p-8 md:p-16 lg:p-2">
-        <div className="relative my-10">
-          <img src="./assets/logo.png" alt="Logo" className="w-40 h-auto" />
-        </div>
-        <div className="flex flex-col gap-4 md:flex-row md:justify-center">
-          <div className="justify-center items-center mb-8 md:mb-0 md:w-2/4">
-            <h3 className="text-lg font-semibold mb-2">Our Mission</h3>
-            <p className="max-w-sm">
-              We help our clients succeed in their crypto mining operations by
-              finding unique purchase opportunities, providing expert industry
-              insight, and offering an elevated customer experience.
-            </p>
-          </div>
-          <div className="mb-8 md:mb-0 md:w-1/4">
-          <a href=""><h3 className="text-lg font-semibold mb-2">Hardware</h3></a>
-          <a href=""><h3 className="text-lg font-semibold mb-2">Solutions</h3></a>
-          <a href=""><h3 className="text-lg font-semibold mb-2">Our Story</h3></a>
-          </div>
-          <div className="mb-8 md:mb-0 md:w-1/4">
-          <a href=""><h3 className="text-lg font-semibold mb-2">Knowledge Block</h3></a>
-          <a href=""><h3 className="text-lg font-semibold mb-2">Client Support</h3></a>
-          <a href=""><h3 className="text-lg font-semibold mb-2">Contact</h3></a>
-          </div>
-          <div className="md:mb-0 md:w-1/5">
-          <a href=""><h3 className="text-lg font-semibold mb-2">hello@46615</h3></a>
-            <div className="flex pl-4">
-              <a href="">
-              <Icon icon="mingcute:telegram-fill" className="text-black mr-2" />
-              </a>
-              <a href="">
-              <Icon icon="entypo-social:twitter" className="text-black mr-2" />
-              </a>
-              <a href="">
-              <Icon icon="entypo-social:linkedin" className="text-black mr-2" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </Container>
+    <footer className="flex bg-white gap-10 p-8 md:p-16 lg:p-4 ">
+      <a href="/" target="_blank" rel="noreferrer">
+        X
+      </a>
+      <a href="/" target="_blank">
+        LINKEDIN
+      </a>
+      <a href="/" target="_blank">
+        MEDIUM
+      </a>
     </footer>
   );
 }
