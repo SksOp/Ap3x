e40bb4f01fd8f07a8be5e4726e363879ff1d0902
Ayush Kaithwas <ayush.91011@gmail.com>
2024-02-15 23:53:40 +0530

    Refactor Investor component layout and styles

--- src/components/investor.tsx
+++ src/components/investor.tsx
@@ -12,32 +12,32 @@ interface InvestorsProps {
   investors: Investor[];
 }
 
-const Investor: React.FC<InvestorsProps> = ({investors}) => {
+const Investor: React.FC<InvestorsProps> = ({ investors }) => {
   return (
-    <div className="flex flex-col items-center py-4 relative bg-white">
+    <div className="flex flex-col gap-5 items-center py-4 relative bg-white">
       <Slant className="absolute top-0 w-full translate-y-[-49%] " />
       <h1 className="text-xl font-bold mb-4">Investors</h1>
-      <div className="grid grid-cols-4 gap-4 w-[60vw] ">
+      <div className="grid grid-cols-4 gap-20 w-[50vw] ">
         {investors.map((investor, index) => (
-          <div key={investor.id + index} className="overflow-hidden">
-            <a href={investor.href ? investor.href:"#"}>
-            <img
-              draggable={false}
-              className="w-full h-auto max-h-[180]  object-cover transform scale-100 group-hover:scale-110 transition-transform duration-300"
-              src={investor.logoLink}
-              alt={`${index}`}
-            />
+          <div key={investor.id + index} className="flex justify-center">
+            <a className="" href={investor.href ? investor.href : "#"}>
+              <img
+                draggable={false}
+                className="w-16 h-auto hover:scale-125 grayscale hover:grayscale-0 transition-all duration-300 ease-in-out"
+                src={investor.logoLink}
+                alt={`${index}`}
+              />
             </a>
           </div>
         ))}
       </div>
-      <a href="/investments">
+      <a className="my-5" href="/investments">
         <Button className="inline-flex items-center m-4 p-4 h-5">
           <span className="p-1">View More</span>
         </Button>
       </a>
     </div>
   );
-}
+};
 
 export default Investor;
